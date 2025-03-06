44a4b0ce53fe5378e3ff585ff6a27f2e5f969679
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-03-06 12:44:47 +0100

    Create main page with several sub-pages

---
+++ gitdocu.js
@@ -0,0 +1,13 @@
+
+
+const fetchGitWorkflowImage = async () => {
+  const imagePath = "./images/git/Git_Workflow.png"; 
+  const gitDocuContainer = document.getElementById("gitDocuContainer"); // Ersetze mit der tatsächlichen ID
+  
+  if (!gitDocuContainer) {
+    console.error("Container für das Git-Workflow-Bild nicht gefunden!");
+    return;
+  }
+
+  gitDocuContainer.innerHTML = `<img src="${imagePath}" alt="Git Workflow" style="max-width: 100%; height: auto; border: 2px solid #000; border-radius: 8px; box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);">`;
+};
