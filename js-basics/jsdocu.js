fc0faa62849d70f433a07cb5c33cca73e36a0435
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-03-10 15:14:27 +0100

    Create jsdocu.js

---
+++ js-basics/jsdocu.js
@@ -0,0 +1,18 @@
+USERS.find((item)=>{})
+const USERS = [
+  {
+    id: 1,
+    username: "mexx",
+    password: "test123",
+    firstName: "Max",
+    familyName: "Muster",
+  },
+  {
+    id: 2,
+    username: "anna",
+    password: "test1234",
+    firstName: "Anna",
+    familyName: "Musterfrau",
+  },
+];
+
